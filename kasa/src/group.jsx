f829639aa338804336a9f23944bd913f3ac5a3de
devclassrooms <bh0@laposte.net>
2023-03-13 15:33:51 +0100

    r1

---
+++ kasa/src/group.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function group() {
+  return (
+    <div className='thebody'>
+        <div className='section_1'>
+            <p className='text_accueil'>Chez vous, partout et ailleurs</p>
+            <div>
+                <img src='image_mid.png' alt='image montagne'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default group
